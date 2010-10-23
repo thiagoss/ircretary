3f425263f94004fb1bc75d73d9f4c57225057fc9
Thiago Santos <thiago.sousa.santos@collabora.co.uk>
2010-10-23 12:00:44 -0300

    ircbot: Cleanup
    Removing twitter stuff

--- ircbot.py
+++ ircbot.py
@@ -5,8 +5,6 @@ from circuits import Component, Debugger
 from circuits.net.sockets import TCPClient, Connect
 from circuits.net.protocols.irc import IRC, Message, User, Nick, Join
 
-api = twitter.Api(username='test', password='test')
-
 class Bot(Component):
 
     def __init__(self, host, port=6667, channel=None):
@@ -25,24 +23,7 @@ class Bot(Component):
             self.push(Nick("%s_" % self("getNick")), "NICK")
 
     def message(self, source, target, message):
-        if '!twit' in message:
-            message = message.split('!twit')[1]
-            self.count += 1
-
-            try:
-                status = api.PostUpdate(message)
-                message = 'Twitted (' + status.text + ')'
-
-                if self.count > 4:
-                    message += ' (delicious spam: http://www.twitter.com/_lolwtf)'
-                    self.count = 0
-
-            except Exception, e:
-                message = 'Failed to twit! :-(', e
-
-            self.push(Message('#embedded-ufcg', message.strip()), "PRIVMSG")
-
-        #self.push(Message(source[0], message), "PRIVMSG")
+        pass
 
 bot = Bot("irc.freenode.net", channel="bot") + Debugger()
 bot.run()
